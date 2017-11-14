132c0e8f61dc508f6fc49ac39ff0e24dd727231d
SerhiiBilyk <sergiybiluk@gmail.com>
2017-11-14 14:12:37 +0100

    trailing comma removed

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,7 +9,7 @@ var APP_DIR = path.resolve(__dirname, './src');
 module.exports = merge(common, {
 
   entry:{
-     about: [APP_DIR + '/containers/About/About.jsx'],
+     about: [APP_DIR + '/containers/About/About.jsx']
 
    },
   output:{
